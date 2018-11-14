6b94895b6a5d5bc5164f49073525637b087de08a
Bowie Hand <bowielh@hotmail.com>
2018-11-13 21:21:34 -0600

    Clean visuals and add responsive functionality.

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,23 @@
-var rawData = rawData;
+var rawData = rawData2;
+
+var parseTime = d3.timeParse("%b-%Y");
+
+rawData.forEach(function(d) {
+  d.dates.forEach(function(data, index) {
+    d.dates[index] = parseTime(data);
+  })
+  if ((d.y === "GDP-Total($B)") || (d.y === "GDP-Retail($B)") || (d.y === "GDP-Tourism($B)") || (d.y === "Employment(M)") || (d.y === "Population(M)")) {
+    d.series.forEach(function(data) {
+      data.values.forEach(function(dd, index) {
+        data.values[index] = dd/1000000
+      })
+    })
+  }
+});
 
 // Initial Params
-var metricSelection = "Employment (M)"
-var stateSelection = ["Alabama"]
+var metricSelection = "Employment(M)"
+var stateSelection = ["Alaska", "California", "Colorado", "Maine", "Massachusetts", "Nevada", "Oregon", "Vermont", "Washington", "Wash D.C."]
 
 var metricIndex = rawData.findIndex(d => d.y === metricSelection)
 var stateIndex = rawData[metricIndex].series.findIndex(d => d.name === stateSelection)
@@ -11,7 +26,7 @@ var svgWidth = 1350;
 var svgHeight = 600;
 
 var margin = {
-  top: 50,
+  top: 100,
   right: 100,
   bottom: 50,
   left: 75
@@ -23,28 +38,49 @@ var height = svgHeight - margin.top - margin.bottom;
 var svg = d3
   .select("#line")
   .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 1350 600")
+  .classed("svg-content", true);
+
+  // .attr("width", svgWidth)
+  // .attr("height", svgHeight);
 
 // Append an SVG group
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .classed("chart-content",true)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+  .attr("width", 100)
 
 // Create user selection groups for metric and state
-var metricSelectionGroup = chartGroup.append("g")
+var metricSelectionGroup = svg.append("g")
   .classed("metric-group", true)
-  .attr("transform", `translate(${width}, ${height + 20})`);
+  .attr("transform", `translate(${svgWidth-50}, ${svgHeight-25})`);
 
-var stateSelectionGroup = chartGroup.append("g")
+var stateSelectionGroup = svg.append("g")
   .classed("state-group", true)
-  .attr("transform", `translate(${width / 2}, ${height + 20})`);
+  .attr("transform", `translate(${(svgWidth-250)/2}, ${svgHeight-10})`);
+
+var stateSelectionGroup2 = svg.append("g")
+  .classed("state-group", true)
+  .attr("transform", `translate(${(svgWidth/2)-25}, ${svgHeight-530})`);
+
+function rebuildData(metricSelection, stateSelection){
+  var newMetricIndex = rawData.findIndex(d => d.y === metricSelection)
+  var newStateIndex = rawData[metricIndex].series.findIndex(d => d.name === stateSelection)
+  var newMetricData = rawData.slice(newMetricIndex,newMetricIndex + 1)
+  var newStateData = newMetricData[0].series.filter(d => stateSelection.includes(d.name))
+
+  var newData = [{y: metricSelection, series: newStateData, dates: rawData[newMetricIndex].dates}]
+
+  buildGraph(newData, newMetricIndex, newStateIndex)
+}
 
 // Loop through data to population metric selection group
 for (i = 0; i < rawData.length; i++) {
   if (rawData[i].y === metricSelection) {
     metricSelectionGroup.append("text")
-      .attr("x", (-svgWidth/2) + i*200)
-      .attr("y", -svgHeight + margin.top)
+      .attr("x", -svgWidth + 190 + i*185)
+      .attr("y", -svgHeight + margin.top - 25)
       .attr("value", rawData[i].y) // value to grab for event listener
       .classed("metric-group", true)
       .text(rawData[i].y)
@@ -52,15 +88,15 @@ for (i = 0; i < rawData.length; i++) {
     }
   else {
     metricSelectionGroup.append("text")
-      .attr("x", (-svgWidth/2) + i*200)
-      .attr("y", -svgHeight + margin.top)
+      .attr("x", -svgWidth + 190 + i*185)
+      .attr("y", -svgHeight + margin.top - 25)
       .attr("value", rawData[i].y) // value to grab for event listener
       .classed("metric-group", true)
       .text(rawData[i].y);
   }};
 
-// Loop through states to population state selection group
-for (j = 0; j < rawData[0].series.length; j++) {
+// Loop through states to population 1st state selection group
+for (j = 0; j < 26; j++) {
   if (stateSelection.includes(rawData[0].series[j].name)) {
     stateSelectionGroup.append("text")
       .attr("x", margin.right + 500)
@@ -79,8 +115,44 @@ for (j = 0; j < rawData[0].series.length; j++) {
       .text(rawData[0].series[j].name)
   }};
 
+// Loop through states to population 2nd state selection group
+for (j = 26; j < rawData[0].series.length; j++) {
+  if (stateSelection.includes(rawData[0].series[j].name)) {
+    stateSelectionGroup2.append("text")
+      .attr("x", margin.right + 500)
+      .attr("y", 0 - svgHeight+margin.top + j*20)
+      .attr("value", rawData[0].series[j].name) // value to grab for event listener
+      .classed("state-group", true)
+      .text(rawData[0].series[j].name)
+      .style("fill","red");
+    }
+  else {
+    stateSelectionGroup2.append("text")
+      .attr("x", margin.right + 500)
+      .attr("y", 0 - svgHeight+margin.top + j*20)
+      .attr("value", rawData[0].series[j].name) // value to grab for event listener
+      .classed("state-group", true)
+      .text(rawData[0].series[j].name)
+  }};
+
 // Event listners
 stateSelectionGroup.selectAll("text")
+  .on("click", function() {
+    var value = d3.select(this).attr("value");
+
+    if (stateSelection.includes(value)) {
+      stateSelection = stateSelection.filter(function removeData(data) {return data !== value});
+      d3.select(this).style("fill","black");
+    }
+    else {
+      stateSelection.push(value);
+      d3.select(this).style("fill","red");
+    };
+
+  rebuildData(metricSelection, stateSelection)
+  });
+
+stateSelectionGroup2.selectAll("text")
   .on("click", function() {
     var value = d3.select(this).attr("value");
 
@@ -109,16 +181,6 @@ metricSelectionGroup.selectAll("text")
   rebuildData(metricSelection, stateSelection)
   });
 
-function rebuildData(metricSelection, stateSelection){
-  var newMetricIndex = rawData.findIndex(d => d.y === metricSelection)
-  var newStateIndex = rawData[metricIndex].series.findIndex(d => d.name === stateSelection)
-  var newMetricData = rawData.slice(newMetricIndex,newMetricIndex + 1)
-  var newStateData = newMetricData[0].series.filter(d => stateSelection.includes(d.name))
-  var newData = [{y: metricSelection, series: newStateData, dates: rawData[newMetricIndex].dates}]
-
-  buildGraph(newData, newMetricIndex, newStateIndex)
-}
-
 function buildGraph(buildData, metricIndex, stateIndex) {
   d3.select(".x-axis").remove()
   d3.select(".y-axis").remove()
@@ -126,11 +188,13 @@ function buildGraph(buildData, metricIndex, stateIndex) {
 
   var xTimeScale1 = d3.scaleTime()
     .domain(d3.extent(buildData[0].dates))
-    .range([0, width]);
+    .range([0, width-150]);
 
   var yLinearScale1 = d3.scaleLinear()
     .domain([0, d3.max(buildData[0].series, d => d3.max(d.values))]).nice()
     .range([height, 0]);
+
+    // d3.min(buildData[0].series, d => d3.max(d.values))*.75
 
   // Create initial axis functions
   var bottomAxis = d3.axisBottom(xTimeScale1);
@@ -140,7 +204,7 @@ function buildGraph(buildData, metricIndex, stateIndex) {
   var xAxis = chartGroup.append("g")
     .classed("x-axis", true)
     .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+    .call(bottomAxis.ticks(width/75).tickSizeOuter(0));
 
   // append y axis
   var yAxis = chartGroup.append("g")
@@ -149,7 +213,8 @@ function buildGraph(buildData, metricIndex, stateIndex) {
 
   // create group for data
   var dataGroup = chartGroup.append("g")
-    .classed("data-group", true);
+    .classed("data-group", true)
+    .attr("fill","none")
 
   var line = d3.line()
     .defined(d => !isNaN(d))
@@ -164,8 +229,5 @@ function buildGraph(buildData, metricIndex, stateIndex) {
     .attr("d", d => line(d.values))
     .classed("line green", true);
 }
-
-var parseTime = d3.timeParse("%Y");
-
 
 rebuildData(metricSelection, stateSelection);
