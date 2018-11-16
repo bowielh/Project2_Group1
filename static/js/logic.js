d3d93c82152e36b6db1f7535d433a98bbb2f7e91
Shweta Banerjee <shwetabanerjee@shwetas-mbp.attlocal.net>
2018-11-16 01:56:53 -0600

    Updated the index.html.

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,53 +11,79 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Define a markerSize function that will give each city a different radius based on its population
-function markerSize(population) {
-  return population / 40;
-}
-
-// Each city object contains the city's name, location and population
-var cities = [
+//List of states that have legalized Recreational Marijuana.
+// Each state object contains the state's name, location, population and GDP 
+var states = [
   {
-    name: "New York",
-    location: [40.7128, -74.0059],
-    GDP: 30,
-    Taxes: "20%"
+    name: "Alaska",
+    location: [64.2008,-149.4937],
+    population: "739,795",
+    GDP: "44.52 billion USD"
   },
   {
-    name: "Chicago",
-    location: [41.8781, -87.6298],
-    GDP: 10,
-    Taxes: "20%"
+    name: "California",
+    location: [36.7783,-119.4179],
+    population: "39.54 million",
+    GDP: "2.448 trillion USD"
   },
   {
-    name: "Houston",
-    location: [29.7604, -95.3698],
-    GDP: 8,
-    Taxes: "20%"
+    name: "Colorado",
+    location: [39.5501,-105.7821],
+    population: "5.607 million",
+    GDP: "295.3 billion USD"
   },
   {
-    name: "Los Angeles",
-    location: [34.0522, -118.2437],
-    GDP: 10,
-    Taxes: "20%"
+    name: "Maine",
+    location: [45.2538,-69.4455],
+    population: "1.336 million",
+    GDP: "48.11 billion USD"
   },
   {
-    name: "Omaha",
-    location: [41.2524, -95.9980],
-    GDP: 12,
-    Taxes: "20%"
+    name: "Massachusetts",
+    location: [42.4072,-71.3824],
+    population: "6.86 million",
+    GDP: "351.5 billion USD"
+  },
+  {
+    name: "Nevada",
+    location: [38.8026,-116.4194],
+    population: "2.998 million",
+    GDP: "127.2 billion USD"
+  },
+  {
+    name: "Oregon",
+    location: [43.8041,-120.5542],
+    population: "4.143 million",
+    GDP: "158.2 billion USD"
+  },
+  {
+    name: "Vermont",
+    location: [44.5588,-72.5778],
+    population: "623,657",
+    GDP: "24.54 billion USD"
+  },
+  {
+    name: "Washington",
+    location: [47.7511,-120.7401],
+    population: "7.406 million",
+    GDP: "311.3 billion USD"
+  },
+  {
+    name: "The District of Columbia",
+    location: [38.9072,-77.0369],
+    population: "693,972",
+    GDP: "93.82 billion USD"
   }
 ];
 
-// Loop through the cities array and create one marker for each city object
-for (var i = 0; i < cities.length; i++) {
-  L.circle(cities[i].location, {
+// Loop through the states array and create one marker for each state object
+for (var i = 0; i < states.length; i++) {
+  L.circle(states[i].location, {
     fillOpacity: 0.75,
-    color: "white",
-    fillColor: "purple",
+    color: "purple",
+    fillColor: "green",
     // Setting our circle's radius equal to the output of our markerSize function
     // This will make our marker's size proportionate to its population
-    radius: markerSize(cities[i].GDP)
-  }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+    radius: 100000
+  }).bindPopup("<h1>" + states[i].name + "</h1> <hr> <h3>Population: " + states[i].population + "</h3>"+"</h1> <hr> <h3>GDP: " + states[i].GDP + "</h3>").addTo(myMap);
 }
